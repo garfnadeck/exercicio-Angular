3f6cd5931cf30e02ae12c23f2556a0f87d40cf33
garfnadeck <serraopinto@gmail.com>
2021-06-08 17:55:04 -0400

    Exercício Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EmployeeComponent} from "./employee/employee.component";
+import {EmployeeItemComponent} from "./employee/employee-item/employee-item.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'employee', component: EmployeeComponent},
+  {path: 'employee/:action', component: EmployeeItemComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeItemComponent } from './employee/employee-item/employee-item.component';
+import {EmployeeService} from "./services/employee-service";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeeItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    EmployeeService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
